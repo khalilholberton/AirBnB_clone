5af68190d70a1120e85912641778317882d6b434
amine784 <1066@holbertonschool.com>
2020-02-17 10:36:23 +0100

    next

--- models/base_model.py
+++ models/base_model.py
@@ -6,28 +6,35 @@ from datetime import datetime
 
 class BaseModel:
     '''that defines all common attributes/methods for other classes'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    value = self.__class__.__name__
+                elif key == 'created_at':
+                    value = self.created_at.isoformat()
+                elif key == 'updated_at':
+                    value = self.updated_at.isoformat()
+                else:
+                    setattr(self, key, value)
 
     def to_dict(self):
         dic = {}
         for key, value in self.__dict__.items():
             if key != 'created_at' and key != 'updated_at':
                 dic[key] = value
-        
+
         dic['__class__'] = self.__class__.__name__
         dic['created_at'] = self.created_at.isoformat()
         dic['updated_at'] = self.updated_at.isoformat()
-       
         return dic
-    
 
     def save(self):
         self.updated_at = datetime.now()
 
-    
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
