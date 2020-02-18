7e41e62d6cd341ac858f0fa322e25c5e41e3f4ab
amine784 <1066@holbertonschool.com>
2020-02-18 18:35:45 +0100

    holberto

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,38 +1,44 @@
 #!/usr/bin/python3
-'''class file storage'''
+''' create class to store object'''
+
+
 import json
+import models
+from models.base_model import BaseModel
+from datetime import datetime
 
 
 class FileStorage:
-    '''create class'''
-    __file_path = 'file.json'
+    ''' class file storage'''
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
         '''funct all'''
-        return(self.__objects)
+        return (FileStorage.__objects)
 
     def new(self, obj):
-        ''' dic form '''
+        ''' dic form'''
         key = '{}.{}'.format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
         '''save'''
+        save_file = self.__file_path
         dic = {}
         for key, value in self.__objects.items():
             dic[key] = value.to_dict()
-        with open(self.__file_path, "w", encoding='utf-8') as f:
-            json.dump(dic, f)
+        with open(save_file, "w", encoding='utf-8') as F:
+            json.dump(dic, F)
 
     def reload(self):
-        '''reload'''
-        rload = {}
+        '''relod'''
+        dic = {}
         try:
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as F:
-                rload = (json.load(F))
-                for key, value in rload.items():
-                    ob = eval(value['__class__'](**value))
+            with open(FileStorage.__file_path, "r") as f:
+                dic = json.load(f)
+                for key, value in dic.items():
+                    ob = eval(value['__class__'])(**value)
                     self.__objects[key] = ob
         except:
             pass
