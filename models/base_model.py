31032849aec05e429d0d9f07671f95eede4bb279
amine784 <1066@holbertonschool.com>
2020-02-18 07:41:28 +0100

    tst

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,50 @@
 #!/usr/bin/python3
-'''create class basemodel'''
+"""
+Define: BaseModel Class
+"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
+
+
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    '''that defines all common attributes/methods for other classes'''
+    """
+    """
     def __init__(self, *args, **kwargs):
+        """
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
-                if key == ['created_at']:
-                    value = self.created_at.isoformat()
-                if key == ['updated_at']:
-                    value = self.updated_at.isoformat()
-                if key not in ['__class__']:
-                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            storage.new(self)
-
-    def to_dict(self):
-        dic = {}
-        for key, value in self.__dict__.items():
-            if key != 'created_at' and key != 'updated_at':
-                dic[key] = value
-
-        dic['__class__'] = self.__class__.__name__
-        dic['created_at'] = self.created_at.isoformat()
-        dic['updated_at'] = self.updated_at.isoformat()
-        return dic
+            models.storage.new(self)
+        
+    def __str__(self):
+        """
+        """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        """
         self.updated_at = datetime.now()
+        models.storage.save()
 
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+
+    def to_dict(self):
+        """
+        """
+        cl_dict = self.__dict__.copy()
+        cl_dict["created_at"] = self.created_at.isoformat()
+        cl_dict["updated_at"] = self.updated_at.isoformat()
+        cl_dict["__class__"] = self.__class__.__name__
+        return cl_dict
