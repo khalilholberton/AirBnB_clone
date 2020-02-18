06e8c7856368dd05bcd72d5566bffad13982f353
amine784 <55138513+amine784@users.noreply.github.com>
2020-02-18 14:35:41 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unittest for Console"""
+'''test console'''
 
 
 from console import HBNBCommand
@@ -9,18 +9,15 @@ from unittest.mock import create_autospec
 
 
 class Test_Console(unittest.TestCase):
-    """Console Unittest"""
+    '''Console '''
 
     def setUp(self):
-        """Sets up STDIN and STDOUT"""
-        self.mock_stdin = create_autospec(sys.stdin)
-        self.mock_stdout = create_autospec(sys.stdout)
+        '''setup'''
+        
 
     def create(self, server=None):
-        """Creates HBNBCommand"""
-        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
+        '''create console'''
+        
 
     def test_quit(self):
-        """Tests the quit command"""
-        xit = self.create()
-        self.assertTrue(xit.onecmd("quit"))
+        '''test quit'''
