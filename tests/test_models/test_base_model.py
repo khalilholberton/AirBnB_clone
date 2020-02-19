e1fd40c94903bcb54b8f3c1010f15d008763650c
amine784 <1066@holbertonschool.com>
2020-02-19 12:59:08 +0100

    holbo

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,31 +3,42 @@
 import unittest
 import os
 from models import *
-import pep8
+import datetime
 
 
 class TestBaseModel(unittest.TestCase):
     '''Test for BaseModel class '''
     def setUp(self):
         '''set'''
-        
+        self.testbase = BaseModel()
 
     def test(self):
         '''test '''
-        
-        
+        basemodel = BaseModel(1)
+        self.assertEqual(hasattr(basemodel, "1"))
+        self.assertEqual(type(basemodel).__name__, "BaseModel")
 
     def type(self):
         '''type'''
-        
 
     def test_save(self):
         '''test save'''
-        
+        testbase = self.testbase.updated_at
+        self.test_model1.save()
+        testbase_save = self.testbase.updated_at
+        self.assertFalse(testbase == testbase_save)
 
     def test_to_json(self):
         '''test to jason'''
-       
+
+    def test_kwarg(self):
+        basemodel = BaseModel(name="amine")
+        self.assertEqual(type(basemodel).__name__, "BaseModel")
+        self.assertFalse(hasattr(basemodel, "id"))
+        self.assertFalse(hasattr(basemodel, "created_at"))
+        self.assertTrue(hasattr(basemodel, "name"))
+        self.assertFalse(hasattr(basemodel, "updated_at"))
+        self.assertTrue(hasattr(basemodel, "__class__"))
 
 if __name__ == '__main__':
     '''__name__'''
