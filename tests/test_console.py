aff894fde00e944debfd6af53a7aba417d9934d1
amine784 <1066@holbertonschool.com>
2020-02-19 12:35:01 +0100

    holberto

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 '''test console'''
-
-
+from unittest.mock import create_autospec
 from console import HBNBCommand
 import sys
 import unittest
-from unittest.mock import create_autospec
 
 
 class Test_Console(unittest.TestCase):
-    '''Console '''
+    '''class to test'''
+    def create(self, server=None):
+        '''freate console'''
+        return(HBNBCommand(stdin=self.mock_stdin,
+                           stdout=self.mock_stdout))
 
     def setUp(self):
         '''setup'''
-        
-    def create(self, server=None):
-        '''create console'''
-        
-    def test_quit(self):
-        '''test quit'''
+        self.mock_stdin = create_autospec(sys.stdin)
+        self.mock_stdout = create_autospec(sys.stdout)
+
+    def test_ex(self):
+        ''' test quit'''
+        exitt = self.create()
+        self.assertTrue(exitt.onecmd("qui"))
+
+    def test_end_of_file(self):
+        '''test end of file'''
+        of = self.create()
+        self.assertTrue(of.onecmd("EOF"))
