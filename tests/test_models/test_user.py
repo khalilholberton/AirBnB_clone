a99ed962538964aabc10a33515276eb60497871d
amine784 <1066@holbertonschool.com>
2020-02-19 14:14:15 +0100

    holbertoo

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,39 +6,39 @@ from models import *
 import datetime
 
 
-class TestState(unittest.TestCase):
-    '''Test for States '''
+class TestUser(unittest.TestCase):
+    '''Test for User '''
     def setUp(self):
         '''set'''
-        self.Statee = State(1)
+        self.Userr = User(1)
 
     def test(self):
         '''test '''
-        Statee = State(1)
-        self.assertEqual(hasattr(Statee, "1"))
-        self.assertEqual(type(Statee).__name__, "State")
+        Userr = User(1)
+        self.assertEqual(hasattr(Userr, "1"))
+        self.assertEqual(type(Userr).__name__, "User")
 
     def type(self):
         '''type'''
 
     def test_save(self):
         '''test save'''
-        Statee = self.Statee.updated_at
-        self.Staate.save()
-        State_save = self.Statee.updated_at
-        self.assertFalse(Statee == Statee_save)
+        Userr = self.User.updated_at
+        self.Useer.save()
+        User_save = self.User.updated_at
+        self.assertFalse(Userr == User_save)
 
     def test_to_json(self):
         '''test to jason'''
 
     def test_kwarg(self):
-        Statee = State(name="amine")
-        self.assertEqual(type(Statee).__name__, "State")
-        self.assertFalse(hasattr(Statee, "id"))
-        self.assertFalse(hasattr(Statee, "created_at"))
-        self.assertTrue(hasattr(Statee, "name"))
-        self.assertFalse(hasattr(Statee, "updated_at"))
-        self.assertTrue(hasattr(Statee, "__class__"))
+        Userr = User(name="amine")
+        self.assertEqual(type(Userr).__name__, "User")
+        self.assertFalse(hasattr(Userr, "id"))
+        self.assertFalse(hasattr(Userr, "created_at"))
+        self.assertTrue(hasattr(Userr, "name"))
+        self.assertFalse(hasattr(Userr, "updated_at"))
+        self.assertTrue(hasattr(Userr, "__class__"))
 
 if __name__ == '__main__':
     '''no interctve mde'''
