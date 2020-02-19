5b06243d7b217dc089211f8f587a5bb799f49caf
amine784 <1066@holbertonschool.com>
2020-02-19 14:43:51 +0100

    holb

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,52 +1,36 @@
 #!/usr/bin/python3
-"""File Storage Unit Tests"""
+"""File Storage test"""
 
 import models
 import os
 import sys
-import pep8
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.amenity import Amenity
-from models.city import City
 from datetime import datetime
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models import *
 
 
 class Test_FileStorage(unittest.TestCase):
-    
-   ''' tst cases class FileStorage''''
-
+    ''' tst cases class FileStorage'''
 
     def test_docstr(self):
-        '''docstr''''
-        
-
-    def test_pep8(self):
-        """Pep8 Test"""
-        
+        '''docstr'''
 
     def set_up(self):
         """
         test setup
         """
 
-
     def test_ins(self):
         """Tests for"""
-        
 
     def test_saves_new_instance(self):
         """Tests create """
-        
 
     def test_all(self):
         """test all"""
-        
 
     def test_new(self):
         """test new"""
