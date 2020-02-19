000a80b707584596efb8a042c24864852109852d
khalilholberton <1071@holbertonschool.com>
2020-02-19 19:52:42 +0000

    fix

--- console.py
+++ console.py
@@ -3,10 +3,15 @@
 this module contain the class HBNBCommand
 """
 import cmd
-import models
 import json
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 from shlex import split
 
 
